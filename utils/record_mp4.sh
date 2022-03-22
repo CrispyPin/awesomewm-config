4fa48087f2aa01c616f33f4a8f7a09b492c092ca
CrispyPin <crispin@tasa.se>
2022-03-22 19:15:24 +0100

    screen recorder outputs android compatible videos, selection is more forgiving for clicking windows

--- utils/record_mp4.sh
+++ utils/record_mp4.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-statusfile="/tmp/screen_recorder_running"
+
+statusfile="/tmp/running_screen_recorder"
+# check for running instance, stop it and exit
 if [ -f $statusfile ]; then
-	ffpid=$(cat $statusfile)
+	ffpid=$(<$statusfile)
 	kill $ffpid
 	rm $statusfile
 	exit 0
 fi
 
-# select area of screen and record it
-slop=$(slop -f "%x %y %w %h %g %i" --color=0.8,0.4,1.0,1.0) || exit 1
+# select area of screen
+slop=$(slop -f "%x %y %w %h %g %i" --color=0.8,0.4,1.0,1.0 -t 16) || exit 1
 read -r X Y W H G ID < <(echo $slop)
 
-videofile=$(xdg-user-dir VIDEOS)/selection_$(date "+%Y.%m.%d-%H:%M:%S").mp4
+videofile=$(xdg-user-dir VIDEOS)/sel_$(date "+%Y.%m.%d-%H:%M:%S").mp4
+logfile="/tmp/screen_recorder_ffmpeg.log"
 
-ffmpeg -y -video_size "$W"x"$H" -f x11grab -framerate 60 -i $DISPLAY+$X,$Y $videofile &
+# make sure selection width and height is even
+# even video size needed for yuv420p format
+# which is needed for android playback & thumbnails in discord
+H=$(( $H - $H%2 ))
+W=$(( $W - $W%2 ))
+
+ffmpeg -y -video_size "$W"x"$H" -f x11grab -framerate 60 \
+-i $DISPLAY+$X,$Y -pix_fmt yuv420p $videofile 2> $logfile &
+
+# store pid so that recording can be stopped next time the script is run
 echo $! > $statusfile
--- utils/screenshot_selection.sh
+++ utils/screenshot_selection.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-maim -s --color=1.0,0.5,0.0,1.0 | xclip -selection clipboard -t image/png
+maim -s --color=1.0,0.5,0.0,1.0 -t 16 | xclip -selection clipboard -t image/png
