9cdf52244dc603fd65d43a5327f6858dfbb4b9e5
CrispyPin <crispin@tasa.se>
2022-05-08 13:04:53 +0200

    separate laptop specific keys

--- modules/special_keys.lua
+++ modules/special_keys.lua
@@ -2,25 +2,9 @@ local awful = require("awful")
 local gears = require("gears")
 local naughty = require("naughty")
 
+require("modules/laptop_detector")
 
 special_keys = gears.table.join(
-	awful.key({}, "XF86MonBrightnessUp", function ()
-		awful.spawn("/usr/bin/simple-brightness -inc 32") end,
-	{description = "Increase brightness", group = "laptop"}),
-
-	awful.key({}, "XF86MonBrightnessDown", function ()
-		awful.spawn("/usr/bin/simple-brightness -dec 32") end,
-	{description = "Lower brightness", group = "laptop"}),
-
-	awful.key({}, "XF86AudioRaiseVolume", function ()
-		awful.spawn("amixer sset Master 5+") end,
-	{description = "Increase volume", group = "laptop"}),
-
-	awful.key({}, "XF86AudioLowerVolume", function ()
-		awful.spawn("amixer sset Master 5-") end,
-	{description = "Lower volume", group = "laptop"}),
-
-
 	awful.key({"Ctrl"}, "Print", function ()
 		awful.util.spawn(CONFIG_DIR .. "utils/screenshot_selection.sh") end,
 	{description = "Full screenshot", group = "screenshot"}),
@@ -38,3 +22,24 @@ special_keys = gears.table.join(
 	{description = "Screenshot active", group = "screenshot"})
 
 )
+
+if is_laptop then
+	special_keys = gears.table.join(
+		special_keys,
+		awful.key({}, "XF86MonBrightnessUp", function ()
+			awful.spawn("/usr/bin/simple-brightness -inc 32") end,
+		{description = "Increase brightness", group = "laptop"}),
+	
+		awful.key({}, "XF86MonBrightnessDown", function ()
+			awful.spawn("/usr/bin/simple-brightness -dec 32") end,
+		{description = "Lower brightness", group = "laptop"}),
+	
+		awful.key({}, "XF86AudioRaiseVolume", function ()
+			awful.spawn("amixer sset Master 5+") end,
+		{description = "Increase volume", group = "laptop"}),
+	
+		awful.key({}, "XF86AudioLowerVolume", function ()
+			awful.spawn("amixer sset Master 5-") end,
+		{description = "Lower volume", group = "laptop"})
+	)
+end
