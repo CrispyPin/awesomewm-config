df675d11a65b3674c92c83c03d43fcaf4c4f39c3
CrispyPin <crispin@tasa.se>
2022-05-08 15:38:57 +0200

    fix autostart for laptop

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -1,22 +1,27 @@
 local awful = require("awful")
 require("modules/smart_reload")
+require("modules/laptop_detector")
 
 if is_reloading() then
 	finish_reload()
 	return
 end
 
-awful.spawn("numlockx on") -- numlock
-awful.spawn(CONFIG_DIR .. "utils/xrandr_conf.sh") -- xrandr config
 awful.spawn(CONFIG_DIR .. "utils/xcape_conf.sh")  -- xcape config
-
 awful.spawn("picom -b") -- compositor
 awful.spawn("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1") -- authentication popup for obs virtual camera etc
 awful.spawn("redshift")
 
-awful.spawn("discord", {screen=3})
-awful.spawn("vscodium")
-awful.spawn("spotify", {tag="2", screen=3, urgent=false})
-awful.spawn("firefox")
+if not is_laptop then
+	awful.spawn("numlockx on") -- numlock
+	awful.spawn(CONFIG_DIR .. "utils/xrandr_conf.sh") -- xrandr config
 
-awful.spawn(terminal .. " -e btop", {tag="3", screen=3, urgent=false})
+	awful.spawn("discord", {screen=3})
+	awful.spawn("vscodium")
+	awful.spawn("spotify", {tag="2", screen=3, urgent=false})
+	awful.spawn("firefox")
+
+	awful.spawn(terminal .. " -e btop", {tag="3", screen=3, urgent=false})
+end
+
+
