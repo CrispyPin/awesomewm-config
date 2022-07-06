a3aa6b0ff7ace9b4f65c82e6ed56f8df5f081e5f
CrispyPin <crispin@tasa.se>
2022-07-06 18:00:06 +0200

    changes

--- modules/navigation_keys.lua
+++ modules/navigation_keys.lua
@@ -15,12 +15,8 @@ navigation_keys = gears.table.join(
 	awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
 			  {description = "focus the previous screen", group = "screen"}),
 
-
-
-	awful.key({ modkey, "Control" }, "t", test,
-			  {description = "focus the previous screen", group = "screen"}),
---	awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
---			  {description = "jump to urgent client", group = "client"}),
+	awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
+			  {description = "jump to urgent client", group = "client"}),
 
 	awful.key({ modkey,           }, "Tab",
 		function ()
